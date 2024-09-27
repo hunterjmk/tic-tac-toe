d4cdb1ec6291a0fdf00043062453b42b9f29c814
hunterjmk <136958839+hunterjmk@users.noreply.github.com>
2024-09-27 09:27:10 +0300

    Create the game board

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('Hello World!');
+/* 
+I'll start by creating the board. It's basically a 2d array. An array of arrays.
+Primary focus is to play this in the console for now. So, I'll be writing with that in mind.
+
+Using an IIFE because I only need the board one!
+*/
+
+const cell = (function(){
+    let value = 0;
+
+    const addToken = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {addToken, getValue};
+})();
+
+const board = (function (){
+    const rows = 3;
+    const columns = rows;
+
+    const gameBoard = [];
+
+    for (let i = 0; i < rows; i++){
+        gameBoard[i] = [];
+        for (let j = 0; j < columns; j++){
+            gameBoard[i].push(cell);
+        }
+    }
+})();
